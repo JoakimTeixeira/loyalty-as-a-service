b97cee09b29e4dddb705c57c0c51d48c1f25c1d2
Joakim Teixeira <setokim1@hotmail.com>
2024-06-09 00:32:02 +0100

    Improve clean script

--- scripts/auth/CleanAllProjects.sh
+++ scripts/auth/CleanAllProjects.sh
@@ -1,45 +1,45 @@
 #!/bin/bash
 
-BASE_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")")
+# Get the directory of the script
+BASE_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 cd "$BASE_DIR"
 
 echo "Cleaning all projects..."
 echo
 
-# Remove all credentials from access.sh file
-sed -i 's/^aws_access_key_id=.*/aws_access_key_id='""'/' scripts/auth/access.sh
-sed -i 's/^aws_secret_access_key=.*/aws_secret_access_key='""'/' scripts/auth/access.sh
-sed -i 's/^aws_session_token=.*/aws_session_token='""'/' scripts/auth/access.sh
-sed -i 's/^export TF_VAR_dockerhub_password=.*/export TF_VAR_dockerhub_password='""'/' scripts/auth/access.sh
-sed -i 's/^export TF_VAR_dockerhub_username=.*/export TF_VAR_dockerhub_username='""'/' scripts/auth/access.sh
+ACCESS_PATH="scripts/auth/Access.sh"
 
-# Remove all credentials from microservices' configuration files
-cd microservices/customer/src/main/resources
-sed -i 's/^quarkus\.container-image\.group=.*/quarkus.container-image.group='""'/' application.properties
-sed -i 's/^quarkus\.datasource\.reactive\.url=.*/quarkus.datasource.reactive.url='""'/' application.properties
-sed -i 's/^kafka\.bootstrap\.servers=.*/kafka.bootstrap.servers='""'/' application.properties
-cd ../../../../../
+# Check if Access.sh exists and remove all credentials if it does
+if [ -f "$ACCESS_PATH" ]; then
+    sed -i "s/^aws_access_key_id=.*/aws_access_key_id=''/g" "$ACCESS_PATH"
+    sed -i "s/^aws_secret_access_key=.*/aws_secret_access_key=''/g" "$ACCESS_PATH"
+    sed -i "s/^aws_session_token=.*/aws_session_token=''/g" "$ACCESS_PATH"
+    sed -i "s/^export TF_VAR_dockerhub_username=.*/export TF_VAR_dockerhub_username=''/g" "$ACCESS_PATH"
+    sed -i "s/^export TF_VAR_dockerhub_password=.*/export TF_VAR_dockerhub_password=''/g" "$ACCESS_PATH"
+else
+    echo "Warning: $ACCESS_PATH not found."
+fi
 
-cd microservices/loyaltycard/src/main/resources
-sed -i 's/^quarkus\.container-image\.group=.*/quarkus.container-image.group='""'/' application.properties
-sed -i 's/^quarkus\.datasource\.reactive\.url=.*/quarkus.datasource.reactive.url='""'/' application.properties
-sed -i 's/^kafka\.bootstrap\.servers=.*/kafka.bootstrap.servers='""'/' application.properties
-cd ../../../../../
+# Function to clean microservice configuration files
+clean_microservice() {
+    local service_dir=$1
+    if [ -f "$service_dir/src/main/resources/application.properties" ]; then
+        sed -i "s/^quarkus\.container-image\.group=.*/quarkus.container-image.group=''/g" "$service_dir/src/main/resources/application.properties"
+        sed -i "s/^quarkus\.datasource\.reactive\.url=.*/quarkus.datasource.reactive.url=''/g" "$service_dir/src/main/resources/application.properties"
+        sed -i "s/^kafka\.bootstrap\.servers=.*/kafka.bootstrap.servers=''/g" "$service_dir/src/main/resources/application.properties"
+    else
+        echo "Warning: $service_dir/src/main/resources/application.properties not found."
+    fi
+}
 
-cd microservices/Purchase/src/main/resources
-sed -i 's/^quarkus\.container-image\.group=.*/quarkus.container-image.group='""'/' application.properties
-sed -i 's/^quarkus\.datasource\.reactive\.url=.*/quarkus.datasource.reactive.url='""'/' application.properties
-sed -i 's/^kafka\.bootstrap\.servers=.*/kafka.bootstrap.servers='""'/' application.properties
-cd ../../../../../
-
-cd microservices/shop/src/main/resources
-sed -i 's/^quarkus\.container-image\.group=.*/quarkus.container-image.group='""'/' application.properties
-sed -i 's/^quarkus\.datasource\.reactive\.url=.*/quarkus.datasource.reactive.url='""'/' application.properties
-sed -i 's/^kafka\.bootstrap\.servers=.*/kafka.bootstrap.servers='""'/' application.properties
-cd ../../../../../
+# Clean configuration files for each microservice
+clean_microservice "microservices/customer"
+clean_microservice "microservices/loyaltycard"
+clean_microservice "microservices/Purchase"
+clean_microservice "microservices/shop"
 
 # Remove all compilation files from Terraform
-find . \( -name ".terraform" -o -name ".terraform.lock.hcl" -o -name ".terraform.tfstate.lock.info" -o -name "terraform.tfstate" -o -name "terraform.tfstate.backup" \) -exec rm -rf {} +
+find . \( -name ".terraform" -o -name ".terraform.tfstate.lock.info" -o -name "terraform.tfstate" -o -name "terraform.tfstate.backup" \) -exec rm -rf {} +
 
 # Remove all compilation files from microservices
 find . \( -name "target" \) -type d -exec rm -rf {} +
