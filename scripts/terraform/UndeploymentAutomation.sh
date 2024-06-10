fbd26799249e8d1ced998710e33f34e81232809f
Joakim Teixeira <setokim1@hotmail.com>
2024-06-10 07:53:21 +0100

    Add validation to check if terraform state folder exists

--- scripts/terraform/UndeploymentAutomation.sh
+++ scripts/terraform/UndeploymentAutomation.sh
@@ -6,49 +6,69 @@ cd "$BASE_DIR"
 source scripts/auth/Access.sh
 source scripts/terraform/utils/ExportVariables.sh
 
+TERRAFORM_STATE_FOLDER=".terraform"
+
 # RDS
 cd terraform/RDS
-terraform destroy -auto-approve
+if [ -d "$TERRAFORM_STATE_FOLDER" ]; then
+    terraform destroy -auto-approve
+fi
 cd ../..
 
 # Kafka
 cd terraform/Kafka
-terraform destroy -auto-approve
+if [ -d "$TERRAFORM_STATE_FOLDER" ]; then
+    terraform destroy -auto-approve
+fi
 cd ../..
 
 # Quarkus Purchase
 cd terraform/Quarkus/Purchase
-terraform destroy -auto-approve
+if [ -d "$TERRAFORM_STATE_FOLDER" ]; then
+    terraform destroy -auto-approve
+fi
 cd ../../..
 
 # Quarkus customer
 cd terraform/Quarkus/customer
-terraform destroy -auto-approve
+if [ -d "$TERRAFORM_STATE_FOLDER" ]; then
+    terraform destroy -auto-approve
+fi
 cd ../../..
 
 # Quarkus shop
 cd terraform/Quarkus/shop
-terraform destroy -auto-approve
+if [ -d "$TERRAFORM_STATE_FOLDER" ]; then
+    terraform destroy -auto-approve
+fi
 cd ../../..
 
 # Quarkus loyaltycard
 cd terraform/Quarkus/loyaltycard
-terraform destroy -auto-approve
+if [ -d "$TERRAFORM_STATE_FOLDER" ]; then
+    terraform destroy -auto-approve
+fi
 cd ../../..
 
 # Quarkus discountcoupon
 cd terraform/Quarkus/discountcoupon
-terraform destroy -auto-approve
+if [ -d "$TERRAFORM_STATE_FOLDER" ]; then
+    terraform destroy -auto-approve
+fi
 cd ../../..
 
 # Quarkus crossselling
 cd terraform/Quarkus/crossselling
-terraform destroy -auto-approve
+if [ -d "$TERRAFORM_STATE_FOLDER" ]; then
+    terraform destroy -auto-approve
+fi
 cd ../../..
 
 # Kong, Konga and Camunda
 cd terraform/Kong
-terraform destroy -auto-approve
+if [ -d "$TERRAFORM_STATE_FOLDER" ]; then
+    terraform destroy -auto-approve
+fi
 cd ../..
 
 # All Quarkus Docker Images
@@ -57,22 +77,26 @@ source scripts/terraform/utils/RemoveDockerImages.sh
 # Key Pair Secrets
 # Kafka Cluster depends on Key Pair Secrets, so the cluster should be destroyed before
 cd terraform/Secrets
-terraform destroy -auto-approve
+if [ -d "$TERRAFORM_STATE_FOLDER" ]; then
+    terraform destroy -auto-approve
+fi
 cd ../..
 
 # S3 Bucket
 # Should be the last to be destroyed because it holds the terraform state from all the resources
 cd terraform/S3
-# Reset S3 bucket state to local backend
-{
-    echo "terraform {"
-    echo "  backend \"local\" {"
-    echo "    path = \"terraform.tfstate\""
-    echo "  }"
-    echo "}"
-} >backend.tf
-echo yes | terraform init -migrate-state
-terraform destroy -auto-approve
+if [ -d "$TERRAFORM_STATE_FOLDER" ]; then
+    # Reset S3 bucket state to local backend
+    {
+        echo "terraform {"
+        echo "  backend \"local\" {"
+        echo "    path = \"terraform.tfstate\""
+        echo "  }"
+        echo "}"
+    } >backend.tf
+    echo yes | terraform init -migrate-state
+    terraform destroy -auto-approve
+fi
 cd ../..
 
 source scripts/auth/CleanAllProjects.sh
--- scripts/terraform/utils/ExportAddresses.sh
+++ scripts/terraform/utils/ExportAddresses.sh
@@ -10,44 +10,66 @@ echo
 echo "Exporting terraform addresses..."
 echo
 
+TERRAFORM_STATE_FOLDER=".terraform"
+
 cd terraform/S3
-export addressS3="$(terraform state show aws_s3_bucket.terraform_state | grep bucket_domain_name | sed "s/bucket_domain_name//g" | sed "s/=//g" | sed "s/\"//g" | sed "s/ //g" | sed "s/$esc\[[0-9;]*m//g")"
+if [ -d "$TERRAFORM_STATE_FOLDER" ]; then
+    export addressS3="$(terraform state show aws_s3_bucket.terraform_state | grep bucket_domain_name | sed "s/bucket_domain_name//g" | sed "s/=//g" | sed "s/\"//g" | sed "s/ //g" | sed "s/$esc\[[0-9;]*m//g")"
+fi
 cd ../..
 
 cd terraform/RDS
-export addressRDS="$(terraform state show aws_db_instance.rds_db | grep address | sed "s/address//g" | sed "s/=//g" | sed "s/\"//g" | sed "s/ //g" | sed "s/$esc\[[0-9;]*m//g")"
+if [ -d "$TERRAFORM_STATE_FOLDER" ]; then
+    export addressRDS="$(terraform state show aws_db_instance.rds_db | grep address | sed "s/address//g" | sed "s/=//g" | sed "s/\"//g" | sed "s/ //g" | sed "s/$esc\[[0-9;]*m//g")"
+fi
 cd ../..
 
 cd terraform/Kafka
-export addresskafka="$(terraform state show 'aws_instance.kafkaCluster[0]' | grep public_dns | sed "s/public_dns//g" | sed "s/=//g" | sed "s/\"//g" | sed "s/ //g" | sed "s/$esc\[[0-9;]*m//g")"
+if [ -d "$TERRAFORM_STATE_FOLDER" ]; then
+    export addresskafka="$(terraform state show 'aws_instance.kafkaCluster[0]' | grep public_dns | sed "s/public_dns//g" | sed "s/=//g" | sed "s/\"//g" | sed "s/ //g" | sed "s/$esc\[[0-9;]*m//g")"
+fi
 cd ../..
 
 cd terraform/Quarkus/Purchase
-export pathPurchase="$(terraform state show aws_instance.purchaseQuarkus | grep public_dns | sed "s/public_dns//g" | sed "s/=//g" | sed "s/\"//g" | sed "s/ //g" | sed "s/$esc\[[0-9;]*m//g")"
+if [ -d "$TERRAFORM_STATE_FOLDER" ]; then
+    export pathPurchase="$(terraform state show aws_instance.purchaseQuarkus | grep public_dns | sed "s/public_dns//g" | sed "s/=//g" | sed "s/\"//g" | sed "s/ //g" | sed "s/$esc\[[0-9;]*m//g")"
+fi
 cd ../../..
 
 cd terraform/Quarkus/customer
-export pathCustomer="$(terraform state show aws_instance.customerQuarkus | grep public_dns | sed "s/public_dns//g" | sed "s/=//g" | sed "s/\"//g" | sed "s/ //g" | sed "s/$esc\[[0-9;]*m//g")"
+if [ -d "$TERRAFORM_STATE_FOLDER" ]; then
+    export pathCustomer="$(terraform state show aws_instance.customerQuarkus | grep public_dns | sed "s/public_dns//g" | sed "s/=//g" | sed "s/\"//g" | sed "s/ //g" | sed "s/$esc\[[0-9;]*m//g")"
+fi
 cd ../../..
 
 cd terraform/Quarkus/shop
-export pathShop="$(terraform state show aws_instance.shopQuarkus | grep public_dns | sed "s/public_dns//g" | sed "s/=//g" | sed "s/\"//g" | sed "s/ //g" | sed "s/$esc\[[0-9;]*m//g")"
+if [ -d "$TERRAFORM_STATE_FOLDER" ]; then
+    export pathShop="$(terraform state show aws_instance.shopQuarkus | grep public_dns | sed "s/public_dns//g" | sed "s/=//g" | sed "s/\"//g" | sed "s/ //g" | sed "s/$esc\[[0-9;]*m//g")"
+fi
 cd ../../..
 
 cd terraform/Quarkus/loyaltycard
-export pathLoyaltyCard="$(terraform state show aws_instance.loyaltyCardQuarkus | grep public_dns | sed "s/public_dns//g" | sed "s/=//g" | sed "s/\"//g" | sed "s/ //g" | sed "s/$esc\[[0-9;]*m//g")"
+if [ -d "$TERRAFORM_STATE_FOLDER" ]; then
+    export pathLoyaltyCard="$(terraform state show aws_instance.loyaltyCardQuarkus | grep public_dns | sed "s/public_dns//g" | sed "s/=//g" | sed "s/\"//g" | sed "s/ //g" | sed "s/$esc\[[0-9;]*m//g")"
+fi
 cd ../../..
 
 cd terraform/Quarkus/discountcoupon
-export pathDiscountCoupon="$(terraform state show aws_instance.discountCouponQuarkus | grep public_dns | sed "s/public_dns//g" | sed "s/=//g" | sed "s/\"//g" | sed "s/ //g" | sed "s/$esc\[[0-9;]*m//g")"
+if [ -d "$TERRAFORM_STATE_FOLDER" ]; then
+    export pathDiscountCoupon="$(terraform state show aws_instance.discountCouponQuarkus | grep public_dns | sed "s/public_dns//g" | sed "s/=//g" | sed "s/\"//g" | sed "s/ //g" | sed "s/$esc\[[0-9;]*m//g")"
+fi
 cd ../../..
 
 cd terraform/Quarkus/crossselling
-export pathCrossSelling="$(terraform state show aws_instance.crossSellingQuarkus | grep public_dns | sed "s/public_dns//g" | sed "s/=//g" | sed "s/\"//g" | sed "s/ //g" | sed "s/$esc\[[0-9;]*m//g")"
+if [ -d "$TERRAFORM_STATE_FOLDER" ]; then
+    export pathCrossSelling="$(terraform state show aws_instance.crossSellingQuarkus | grep public_dns | sed "s/public_dns//g" | sed "s/=//g" | sed "s/\"//g" | sed "s/ //g" | sed "s/$esc\[[0-9;]*m//g")"
+fi
 cd ../../..
 
 cd terraform/Kong
-export pathKongKongaCamunda="$(terraform state show aws_instance.installKongKongaCamunda | grep public_dns | sed "s/public_dns//g" | sed "s/=//g" | sed "s/\"//g" | sed "s/ //g" | sed "s/$esc\[[0-9;]*m//g")"
+if [ -d "$TERRAFORM_STATE_FOLDER" ]; then
+    export pathKongKongaCamunda="$(terraform state show aws_instance.installKongKongaCamunda | grep public_dns | sed "s/public_dns//g" | sed "s/=//g" | sed "s/\"//g" | sed "s/ //g" | sed "s/$esc\[[0-9;]*m//g")"
+fi
 cd ../..
 
 echo "Finished exporting addresses."
